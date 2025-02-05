b394d7f3b236147e14bf829a1e0e745eecaea838
Nathnael <nathnaelzelalem@gmail.com>
2025-02-05 17:01:33 +0300

    Finished Modal

--- lib/models/expenses.dart
+++ lib/models/expenses.dart
@@ -1,13 +1,20 @@
 import "package:uuid/uuid.dart";
 
-final uuid = const Uuid();
+const uuid = Uuid();
+
+enum Category { food, travel, leuisure, work }
 
 class Expenses {
-  Expenses({required this.title, required this.amount, required this.date})
+  Expenses(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
       : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }

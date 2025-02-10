01c9e70f5488700e397ab1b4f4e345424b965540
Nathnael <nathnaelzelalem@gmail.com>
2025-02-10 14:49:40 +0300

    Add Dark Mood

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,35 @@ import 'package:expense_tracker/widgets/expense.dart';
 import 'package:flutter/material.dart';
 
 var KColorScheme =
-    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 48, 14, 126));
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 42, 5, 128));
 
+var KDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark, seedColor: Color.fromARGB(255, 5, 99, 125));
 void main() {
   runApp(MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+          colorScheme: KDarkColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: KDarkColorScheme.primary,
+            // foregroundColor: KColorScheme.onPrimary,
+          ),
+          cardTheme: CardTheme().copyWith(
+            color: KDarkColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 7),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+            backgroundColor: KDarkColorScheme.primaryContainer,
+          )),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: KDarkColorScheme.onSecondaryContainer),
+              )),
       theme: ThemeData().copyWith(
           colorScheme: KColorScheme,
           appBarTheme: AppBarTheme().copyWith(
@@ -29,6 +54,7 @@ void main() {
                     fontWeight: FontWeight.bold,
                     color: KColorScheme.onSecondaryContainer),
               )),
+      themeMode: ThemeMode.system,
       home: Expense()));
 }
 // lecture 05/10
